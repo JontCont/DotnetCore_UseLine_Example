56db895435d1b960145ef2f58abf5e97134c86c4
jontcont <52921053+JontCont@users.noreply.github.com>
2023-03-10 09:51:45 +0800

    initial source

---
+++ Extensions/ConfigExetensions.cs
@@ -0,0 +1,37 @@
+﻿namespace start5M.Line.WebAPI.Extensions
+{
+    public static class Config
+    {
+        /// <summary>
+        /// 取得 ConnectionStrings 底下資料
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public static string GetConnectionString(string name)
+        {
+            IConfiguration config = GetConfiguration();
+            return config.GetConnectionString(name);
+        }
+
+        /// <summary>
+        /// 取用  appsettings.json 資料
+        /// </summary>
+        /// <returns></returns>
+        public static IConfiguration GetConfiguration()
+        {
+            var assembly = AppDomain.CurrentDomain
+                .GetAssemblies()
+                .Single(o => o.EntryPoint != null);
+
+            IConfiguration config = new ConfigurationBuilder()
+                          .SetBasePath(Directory.GetCurrentDirectory())
+                          .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                          .AddUserSecrets(assembly, optional: false)
+                          .Build();
+            Console.WriteLine(config["Test"]);
+
+            return config;
+        }
+
+    }
+}
