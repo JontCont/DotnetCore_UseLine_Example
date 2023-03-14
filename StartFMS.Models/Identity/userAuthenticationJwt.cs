af69231aaf67014ab1286ff4551d7b8dbe70883a
jontcont <52921053+JontCont@users.noreply.github.com>
2023-03-14 18:17:48 +0800

    Revert "Revert "移動專案""
    This reverts commit 8e0621ef0a0e1313611a305f0d0a6a850a92f3c6.

---
+++ StartFMS.Models/Identity/userAuthenticationJwt.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartFMS.Models.Identity;
+public partial class userAuthenticationJwt {
+    public string Org { get; set; }
+    public string User { get; set; }
+    public string Token { get; set; }
+    public string Roles { get; set; }  // for bdp090A 權限
+    public string SysCode { get; set; }
+}
